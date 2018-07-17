b9e5af186b51e581a9e8b3aa1a6c58a60b1d207a
Uma Desai <umadesai38@gmail.com>
2018-07-17 18:15:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World"
+  end
+  
+get '/name' do
+    "My name is Uma Desai"
+end
+
+get '/hometown' do
+  "My hometown is Los Alamos"
+end
+
+get '/favorite-song' do
+  "My favorite song is The Weekend by SZA"
+end
 end
