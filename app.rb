fea7739953819406dfc7dae1fab5edcaa2f66ce3
darcychung <darcycchung@gmail.com>
2018-07-17 18:19:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::Base 
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Darcy."
+  end
+  
+  get '/hometown' do
+    "My hometown is Newport Beach."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Speak Now."
+  end
 end
