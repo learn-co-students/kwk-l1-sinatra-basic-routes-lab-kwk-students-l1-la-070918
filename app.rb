a46cd41a62df000ed005c802db356857d03d9cdd
Ashyah Hewage <ashyahgalbokke@gmail.com>
2018-07-17 18:19:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Ashyah"
+  end
+  
+  get '/hometown' do
+    "My hometown is Santa Monica."
+  end
+  
+   get '/favorite-song' do
+    "My favorite song is Shape of You by Ed Sheeran. "
+  end
 end
